4c806356134f4c629d2a3fbe0e04cc9bb9603b85
Saurabh Pingale <saurabhpingale93@gmail.com>
2024-08-22 12:46:15 +0530

    Implemented Home

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+const Home = () => {
+  return (
+    <Container>
+      <Typography variant="h2" gutterBottom>
+        Welcome to Totality Property Rentals
+      </Typography>
+      <Typography variant="h6">
+        Find your perfect rental property!
+      </Typography>
+    </Container>
+  );
+};
+
+export default Home;
